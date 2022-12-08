68c7eff50bc46e0c8764a777a9e2430628c9b598
TankVivek <112782531+TankVivek@users.noreply.github.com>
2022-12-08 10:45:43 +0530

    Delete Tops HTML directory

--- Tops HTML/int weight=46.23;.c
+++
@@ -1,9 +0,0 @@
-int weight = 46.23;
-int num;
-float age;
-char first_letter;
-printf("enter number age and firstletter");
-scanf("%d %f %c ,&num, &age, &first_letter");
-printf("value of num is %d" num);
-printf("value of age is %f", age);
-printf("value of first_letter is %c", first_letter);
