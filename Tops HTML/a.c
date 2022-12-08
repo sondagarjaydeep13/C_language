e8ab3f8c88a1777a0aeab1c7af6a9674986a692b
TankVivek <112782531+TankVivek@users.noreply.github.com>
2022-12-08 10:45:19 +0530

    Add files via upload

---
+++ Tops HTML/a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   // printf("%d \n", 2>3 || 1>2);
+   int x;
+   printf("enter a number :");
+   scanf("%d" ,&x);
+   printf("%d", x % 2 == 0);
+  // printf("%d \n", 44%3);
+return 0;
+}
