27a195130d0071b1ad771185b7f39bd2bd1871a8
TankVivek <vivektank136@gmail.com>
2022-12-31 14:57:09 -0500

    codes cpp

---
+++ codes/1.  Write a program for the following one/clear_a_bit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+
+{
+    int num,pos;
+    printf("enter any number \n");
+    scanf("%d",&num);
+    printf("enter postion which you want to set:\n");
+    scanf("%d",&pos);
+
+    printf("befoer setting the number:%d\n",num);
+    num=num|1<
+
+
+
+}
+ 
