bdec09e36d8f68e314be2e58fa1e7e5b8ea0c7c6
TankVivek <vivektank136@gmail.com>
2023-01-01 10:33:55 -0500

    codes cpp

---
+++ codes/1.  Write a program for the following one/set_a_bit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+
+{
+    int num,pos;
+    printf("enter any number \n");
+    scanf("%d",&num);
+    printf("enter postion which you want to set:\n");
+    scanf("%d",&pos);
+
+    printf("befoer setting the number:%d\n",num);
+    num=-num;
+    
+    return 0;
+
+
+
+}
+ 
