1425e2a660a32125ecebf88b15bae2751b0758b3
TankVivek <vivektank136@gmail.com>
2023-01-05 12:29:29 -0500

    codes cpp

--- codes/1.  Write a program for the following one/clear_a_bit.cpp
+++ codes/1.  Write a program for the following one/clear_a_bit.cpp
@@ -1,1 +1,27 @@
-a
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    
+int num, pos;
+
+cout<<"enter the number.."<<endl;
+
+cin>>num;
+
+cout<<"enter the position..,"<<endl;
+
+cin>>pos;
+
+cout<<"Before Number = " << num <<endl;                      
+ 
+num = num &~ (1<<pos);
+
+cout<<"after number = " << num <<endl;       
+
+
+return 0;
+}
+
