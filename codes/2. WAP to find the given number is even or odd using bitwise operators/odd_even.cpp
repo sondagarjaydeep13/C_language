033755720db5306d0f708a470f0d45d0cb473322
TankVivek <vivektank136@gmail.com>
2023-01-17 07:05:11 -0500

    codes cpp

---
+++ codes/2. WAP to find the given number is even or odd using bitwise operators/odd_even.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+
+{  
+    int no;
+    
+    cout<<"enter the number"<<endl;
+    cin>>no;
+
+    if (no & 1)
+
+    { cout<<"odd"<<endl; }
+
+else // (number & 1) == 0
+
+    { cout<<"even"<<endl; } 
+     
+
+}
