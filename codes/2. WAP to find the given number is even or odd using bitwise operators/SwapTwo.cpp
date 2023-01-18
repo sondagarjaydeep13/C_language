3ffbded29ad0d3c957e0c2cb82b058d2721c66b2
TankVivek <vivektank136@gmail.com>
2023-01-18 06:11:37 -0500

    codes cpp

--- codes/2. WAP to find the given number is even or odd using bitwise operators/SwapTwo.cpp
+++ codes/2. WAP to find the given number is even or odd using bitwise operators/SwapTwo.cpp
@@ -13,7 +13,7 @@ int main()
   y = x^y;
   x = x^y;
    
-  cout << "After swapping x = " << x << " and y = " << y << ".\n";
+  cout << "After swapping x = " << x << " and y = " << y << ".
   
   return 0;
 }
