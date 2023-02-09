575d49a7524a45da6a0dbc3f69c0f24edd8a3ebf
TankVivek <vivektank136@gmail.com>
2023-02-09 15:18:09 -0500

    C language

---
+++ C language modul/ASSIGNMENT 3.2/BAISIC_CALCULATOR/basic_calculation.c
@@ -0,0 +1,26 @@
+
+//  baisic calculator
+
+#include<stdio.h>
+
+int main ()
+
+{
+
+int a,b,sum;
+  
+printf("enter value of a");
+scanf("%d",&a);
+printf("enter value of b");
+scanf("%d",&b);
+sum= a/b;
+printf("this is addititon %d",sum);
+
+return 0;
+
+}
+  
+
+  
+
+
