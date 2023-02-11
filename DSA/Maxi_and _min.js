29dad20ef8c367d61101b1c9797f6ab0d10c69f5
TankVivek <vivektank136@gmail.com>
2023-02-11 10:02:23 -0500

    JavaScript

--- DSA/Maxi_and _min.js
+++ DSA/Maxi_and _min.js
@@ -1,35 +1,31 @@
 
 
 var i , min , max ;
- 
-a = [1,2,3,4,5,]
- max = a[0]
 
-for(i = 0 ; i < a.length ; i++)
-{
-if(a[0] > max)
-{
-   max = a[i]
+var a = [1,2,3,4,5]
 
-}}
+    max = a[0]
 
-min = a[0]
-
-for(i = 0 ; i < a.length ; i++)
-{
-if (a[0] < min){
-
-    min = a[i]
-
-}}
-
-console.log(max);
-console.log(min);
-
-
-
-
-
-
-
+    for(i = 0 ; i < a.length ; i++)
+     {
+    if(a[0] > max)
+    {
+        max = a[i]
+   
+    }}
+    
+    
+    min = a[0]
+    
+    for(i = 0 ; i < a.length ; i++)
+    {
+        if (a[0] < min){
+            
+            min = a[i]
+            
+        }}
+        
+        
+        console.log(min);
+        console.log(max);
 
