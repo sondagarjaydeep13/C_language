91ea84dbf6adc973e22d32a2f6fc6cc123e45b1e
TankVivek <vivektank136@gmail.com>
2023-02-11 10:40:41 -0500

    JavaScript

---
+++ DSA/reverse.js
@@ -0,0 +1,37 @@
+  
+            // Reverse Array
+
+// const reverseArray = (Arry,start,end) => 
+// {
+//     while(start< end)
+//     {
+//         var tamp = Arry [start];
+//         Arry[start] = Arry[end];
+//         Arry[end] =tamp;
+//         start++;
+//         end--;
+//     }
+// }
+
+// const PrintArray = (Arry,size) =>
+// {
+//     for (var i = 0; i < size ; i++)
+//     {
+//         console.log(Arry[i]);
+//     }
+// }
+// var Arry = [1,2,3,4,5,5,6];
+// var n = 7;
+
+// PrintArray(Arry,n); 
+
+// reverseArray(Arry,0,n-1);
+
+// console.log("reverse array is");
+
+// PrintArray(Arry,n);
+
+            // Second Mathod.
+
+            
+
